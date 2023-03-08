1a4df3b1236667f6157d77297c61926983954f4d
Berk Erol <berk-erol@outlook.com>
2023-03-08 01:20:10 +0100

    First commit

---
+++ flight-tracker.js
@@ -0,0 +1,64 @@
+const FLIGHTRADAR_URL = 'https://api.codetabs.com/v1/proxy?quest=https://data-live.flightradar24.com/';
+const headers = [['Latitude', 1], ['Longitude', 2], ['Heading', 3], ['Altitude', 4], ['Ground Speed', 5], ['On Ground', 14], ['Vertical Speed', 15], ['Aircraft', 8], ['Registration', 9], ['Origin Airport', 11], ['Destination Airport', 12], ['Flight', 13], ['Call Sign', 16], ['Airline', 18]];
+const tr = document.createElement('tr');
+for (const header of headers) {
+  createElement(tr, 'th', header[0]);
+}
+document.getElementsByTagName('thead')[0].appendChild(tr);
+
+function createElement(tr, type, content) {
+  const element = document.createElement(type);
+  element.innerHTML = content;
+  tr.appendChild(element);
+}
+
+async function getDetails(flightId) {
+  return window.fetch(`${FLIGHTRADAR_URL}clickhandler/?flight=${flightId}`, { headers: { 'Origin': 'https://berkerol.github.io' } })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      const origin = res.airport.origin;
+      const destination = res.airport.destination;
+      return [res.aircraft.model === undefined ? '' : res.aircraft.model.text, res.airline === null ? '' : res.airline.name, origin === null ? '' : origin.name, destination === null ? '' : destination.name];
+    });
+}
+
+async function list() {
+  const params = new URLSearchParams(window.location.search);
+  const coordinates = params.has('bounds') ? params.get('bounds') : '48.4,47.9,11.3,11.8';
+  await window.fetch(`${FLIGHTRADAR_URL}zones/fcgi/feed.js?bounds=${coordinates}&faa=1&satellite=1&malt=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=0&estimated=1&maxage=14400&gliders=0&stats=0`, { headers: { 'Origin': 'https://berkerol.github.io' } })
+    .then(res => {
+      return res.json();
+    })
+    .then(async res => {
+      const tbody = document.getElementsByTagName('tbody')[0];
+      for (const flightId in res) {
+        if (flightId !== 'full_count' && flightId !== 'version') {
+          const flight = res[flightId];
+          const details = await getDetails(flightId);
+          const tr = document.createElement('tr');
+          for (const header of headers) {
+            if (header[1] === 8) {
+              const description = details[0] === '' ? '' : ` - ${details[0]}`;
+              createElement(tr, 'td', `${flight[8]}${description}`);
+            } else if (header[1] === 11) {
+              const description = details[2] === '' ? '' : ` - ${details[2].replace(' Airport', '')}`;
+              createElement(tr, 'td', `${flight[11]}${description}`);
+            } else if (header[1] === 12) {
+              const description = details[4] === '' ? '' : ` - ${details[3].replace(' Airport', '')}`;
+              createElement(tr, 'td', `${flight[12]}${description}`);
+            } else if (header[1] === 18) {
+              const description = details[1] === '' ? '' : (flight[18] === '' ? '' : ' - ') + details[1];
+              createElement(tr, 'td', `${flight[18]}${description}`);
+            } else {
+              createElement(tr, 'td', flight[header[1]]);
+            }
+          }
+          tbody.appendChild(tr);
+        }
+      }
+    });
+}
+
+list();
