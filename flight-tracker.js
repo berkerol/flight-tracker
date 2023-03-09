18d4f263ef75e1ca7d93dc6157d1e7190ca94080
Berk Erol <berk-erol@outlook.com>
2023-03-10 00:15:27 +0100

    Add flag emojis and country names for registration

--- flight-tracker.js
+++ flight-tracker.js
@@ -1,5 +1,6 @@
 const FLIGHTRADAR_URL = 'https://api.codetabs.com/v1/proxy?quest=https://data-live.flightradar24.com/';
 const FIXED_COUNTRY_CODES = [['AFG', 'AF'], ['AGO', 'AG'], ['ARE', 'AE'], ['ARM', 'AM'], ['AUS', 'AU'], ['AUT', 'AT'], ['AZE', 'AZ'], ['BEL', 'BE'], ['BFA', 'BF'], ['BGD', 'BD'], ['BRA', 'BR'], ['CAN', 'CA'], ['CHN', 'CN'], ['CMR', 'CM'], ['COG', 'CG'], ['CPV', 'CV'], ['CYP', 'CY'], ['CZE', 'CZ'], ['DEU', 'DE'], ['DJI', 'DJ'], ['DNK', 'DK'], ['DZA', 'DZ'], ['EGY', 'EG'], ['ESP', 'ES'], ['EST', 'EE'], ['FIN', 'FI'], ['FRA', 'FR'], ['GAB', 'GA'], ['GBR', 'GB'], ['GEO', 'GE'], ['GGY', 'GG'], ['GHA', 'GH'], ['GIN', 'GN'], ['GNQ', 'GQ'], ['GRC', 'GR'], ['GRL', 'GL'], ['HRV', 'HR'], ['HUN', 'HU'], ['IDN', 'ID'], ['IND', 'IN'], ['IRL', 'IE'], ['IRN', 'IR'], ['IRQ', 'IR'], ['ISR', 'IL'], ['ITA', 'IT'], ['JPN', 'JP'], ['KAZ', 'KZ'], ['KEN', 'KE'], ['KGZ', 'KG'], ['KOR', 'KR'], ['LAO', 'LA'], ['LBR', 'LR'], ['LBY', 'LB'], ['LTU', 'LT'], ['MAC', 'MO'], ['MAR', 'MA'], ['MDA', 'MD'], ['MKD', 'MK'], ['MLI', 'ML'], ['MMR', 'MM'], ['MNE', 'ME'], ['MNP', 'MP'], ['MOZ', 'MZ'], ['MUS', 'MU'], ['MYS', 'MY'], ['NAM', 'NA'], ['NER', 'NE'], ['NGA', 'NG'], ['NLD', 'NL'], ['NOR', 'NO'], ['PAK', 'PK'], ['PHL', 'PH'], ['POL', 'PL'], ['PRT', 'PT'], ['QAT', 'QA'], ['ROU', 'RO'], ['RUS', 'RU'], ['RWA', 'RW'], ['SAU', 'SA'], ['SDN', 'SD'], ['SLE', 'SL'], ['SVK', 'SK'], ['SWE', 'SE'], ['TGO', 'TG'], ['THA', 'TH'], ['TJK', 'TJ'], ['TKM', 'TM'], ['TLS', 'TL'], ['TUN', 'TN'], ['TUR', 'TR'], ['TWN', 'TW'], ['TZA', 'TZ'], ['UGA', 'UG'], ['USA', 'US'], ['UZB', 'UZ'], ['VEN', 'VE'], ['VNM', 'VN'], ['ZWE', 'ZW']];
+const REGISTRATION_COUNTRIES = [['4K-', 'AZ', 'Azerbaijan'], ['4L-', 'GE', 'Georgia'], ['4R-', 'LK', 'Sri Lanka'], ['4X-', 'IL', 'Israel'], ['5B-', 'CY', 'Cyprus'], ['6V-', 'SN', 'Senegal'], ['7T-', 'DZ', 'Algeria'], ['9H-', 'MT', 'Malta'], ['9K-', 'KW', 'Kuwait'], ['9M-', 'MY', 'Malaysia'], ['9V-', 'SG', 'Singapore'], ['A4O-', 'OM', 'Oman'], ['A6-', 'AE', 'United Arab Emirates'], ['A7-', 'QA', 'Qatar'], ['A9C-', 'BH', 'Bahrain'], ['AP-', 'PK', 'Pakistan'], ['B-', 'CN', 'China'], ['C-', 'CA', 'Canada'], ['CN-', 'MA', 'Morocco'], ['CS-', 'PT', 'Portugal'], ['D-', 'DE', 'Germany'], ['EC-', 'ES', 'Spain'], ['EI-', 'IE', 'Ireland'], ['EP-', 'IR', 'Iran'], ['ET-', 'ET', 'Ethiopia'], ['EW-', 'BY', 'Belarus'], ['F-', 'FR', 'France'], ['G-', 'GB', 'United Kingdom'], ['HA-', 'HU', 'Hungary'], ['HB-', 'CH', 'Switzerland'], ['HL', 'KR', 'South Korea'], ['HS-', 'TH', 'Thailand'], ['HZ-', 'SA', 'Saudi Arabia'], ['JA', 'JP', 'Japan'], ['I-', 'IT', 'Italy'], ['LX-', 'LU', 'Luxembourg'], ['LZ-', 'BG', 'Bulgaria'], ['M-', 'IM', 'Isle of Man'], ['OH-', 'FI', 'Finland'], ['OK-', 'CZ', 'Czech Republic'], ['OO-', 'BE', 'Belgium'], ['OY-', 'DK', 'Denmark'], ['N', 'US', 'United States of America'], ['OE-', 'AT', 'Austria'], ['PH-', 'NL', 'Netherlands'], ['PK-', 'ID', 'Indonesia'], ['RA-', 'RU', 'Russia'], ['RP-', 'PH', 'Philippines'], ['S2-', 'BD', 'Bangladesh'], ['SE-', 'SE', 'Sweden'], ['SP-', 'PL', 'Poland'], ['ST-', 'SD', 'Sudan'], ['SU-', 'EG', 'Egypt'], ['SX-', 'GR', 'Greece'], ['TC-', 'TR', 'Turkey'], ['UK', 'UZ', 'Uzbekistan'], ['UR-', 'UA', 'Ukraine'], ['VH-', 'AU', 'Australia'], ['VN-', 'VN', 'Vietnam'], ['VP-B', 'BM', 'Bermuda'], ['VT-', 'IN', 'India'], ['YI-', 'IQ', 'Iraq'], ['YK-', 'SY', 'Syria'], ['YL-', 'LV', 'Latvia'], ['YR-', 'RO', 'Romania']];
 const headers = [['Latitude', 1], ['Longitude', 2], ['Heading', 3], ['Altitude', 4], ['Ground Speed', 5], ['On Ground', 14], ['Vertical Speed', 15], ['Aircraft', 8], ['Registration', 9], ['Origin Airport', 11], ['Destination Airport', 12], ['Flight', 13], ['Call Sign', 16], ['Airline', 18], ['Flightradar24 Link', 19]];
 const tr = document.createElement('tr');
 for (const header of headers) {
@@ -13,10 +14,14 @@ function createElement(tr, type, content) {
   tr.appendChild(element);
 }
 
-function getFlagEmoji(countryCode) {
+function fixCountryCode(countryCode) {
   for (const code of FIXED_COUNTRY_CODES) {
     countryCode = countryCode.replace(code[0], code[1]);
   }
+  return countryCode;
+}
+
+function getFlagEmoji(countryCode) {
   const codePoints = countryCode
     .toUpperCase()
     .split('')
@@ -62,11 +67,19 @@ async function list() {
             if (header[1] === 8) {
               const description = details[0] === '' ? '' : ` - ${details[0]}`;
               createElement(tr, 'td', `${flight[8]}${description}`);
+            } else if (header[1] === 9) {
+              let description = '';
+              for (const country of REGISTRATION_COUNTRIES) {
+                if (flight[9].startsWith(country[0])) {
+                  description = ` - ${getFlagEmoji(country[1])} ${country[2]}`;
+                }
+              }
+              createElement(tr, 'td', `${flight[9]}${description}`);
             } else if (header[1] === 11) {
-              const description = details[2] === '' ? '' : ` - ${getFlagEmoji(details[2])} ${details[3].replace(' Airport', '')}`;
+              const description = details[2] === '' ? '' : ` - ${getFlagEmoji(fixCountryCode(details[2]))} ${details[3].replace(' Airport', '')}`;
               createElement(tr, 'td', `${flight[11]}${description}`);
             } else if (header[1] === 12) {
-              const description = details[4] === '' ? '' : ` - ${getFlagEmoji(details[4])} ${details[5].replace(' Airport', '')}`;
+              const description = details[4] === '' ? '' : ` - ${getFlagEmoji(fixCountryCode(details[4]))} ${details[5].replace(' Airport', '')}`;
               createElement(tr, 'td', `${flight[12]}${description}`);
             } else if (header[1] === 18) {
               const description = details[1] === '' ? '' : (flight[18] === '' ? '' : ' - ') + details[1];
